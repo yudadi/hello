aab19ae4b6818a7b69381bdfa6a9d9affe636e06
yudadi <1012913473@qq.com>
2018-06-14 21:35:23 +0800

    修改

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,10 +3,14 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
     public String Hello() {
+        Date date = new Date();
+        System.out.println(date);
         return "Hello SpringBoot!";
     }
 }
